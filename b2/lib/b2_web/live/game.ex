6fa5dcedaa36f30cff1f3a009934231b0561c55f
Jack Middlebrook <jack.middlebrook@gmail.com>
2021-11-21 16:41:57 +0800

    Implement hangman game and styling

--- b2/lib/b2_web/live/game.ex
+++ b2/lib/b2_web/live/game.ex
@@ -10,9 +10,18 @@ defmodule B2Web.Live.Game do
     { :ok, socket }
   end
 
+  def handle_event("make_move", %{ "key" => key }, socket) do
+    tally = Hangman.make_move(socket.assigns.game, key)
+    {:noreply, assign(socket, :tally, tally)}
+  end
+
   def render(assigns) do
     ~L"""
-      <%= inspect assigns.tally %>
+      <div class="game-holder" phx-window-keyup="make_move">
+        <%= live_component(__MODULE__.Figure, tally: assigns.tally, id: 1) %>
+        <%= live_component(__MODULE__.Alphabet, tally: assigns.tally, id: 2) %>
+        <%= live_component(__MODULE__.WordsSoFar, tally: assigns.tally, id: 3) %>
+      </div>
     """
   end
 
